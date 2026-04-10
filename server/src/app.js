3f5af2c737c7e216931720e5d781b8e15267e7c1
Juan David Burbano <juan_dav.burbano@uao.edu.co>
2026-04-10 09:06:19 -0500

    aumentar limite de body a 5mb para envio de imagen

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ app.use(
         credentials: true,
     })
 );
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 app.get("/", (req, res) => {
     res.json({ message: "Backend SLC Turnos OK" });
